38da7eaac349794b7762f45bdabdddccd9838a08
thiago-miranda <thiago.miranda@hotmail.com>
2020-08-05 14:24:06 -0300

    finalizado

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Jogo extends StatefulWidget {
@@ -6,6 +8,72 @@ class Jogo extends StatefulWidget {
 }
 
 class _JogoState extends State<Jogo> {
+  var _imagemApp = AssetImage("images/padrao.png");
+  String _mensagem = "Escolha uma opção abaixo";
+  var _escolhaBorderUsuario;
+  int _vitorias = 0;
+  int _derrotas = 0;
+  int _empate = 0;
+
+  void _resetar() {
+    setState(() {
+      this._vitorias = 0;
+      this._derrotas = 0;
+      this._empate = 0;
+      this._escolhaBorderUsuario = "padrao";
+    });
+  }
+
+  void _opcaoSelecionada(String escolhaUsuario) {
+    var opcoes = ["pedra", "papel", "tesoura"];
+    var numero = Random().nextInt(3);
+    var escolhaApp = opcoes[numero];
+    this._escolhaBorderUsuario = escolhaUsuario;
+
+    switch (escolhaApp) {
+      case "pedra":
+        setState(() {
+          this._imagemApp = AssetImage("images/pedra.png");
+        });
+        break;
+      case "papel":
+        setState(() {
+          this._imagemApp = AssetImage("images/papel.png");
+        });
+        break;
+      case "tesoura":
+        setState(() {
+          this._imagemApp = AssetImage("images/tesoura.png");
+        });
+        break;
+      default:
+        setState(() {
+          this._imagemApp = AssetImage("images/padrao.png");
+        });
+    }
+
+    if ((escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+        (escolhaUsuario == "tesoura" && escolhaApp == "papel") ||
+        (escolhaUsuario == "papel" && escolhaApp == "pedra")) {
+      setState(() {
+        this._mensagem = "Parabénss!! você ganhou! :)";
+        this._vitorias++;
+      });
+    } else if ((escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
+        (escolhaApp == "tesoura" && escolhaUsuario == "papel") ||
+        (escolhaApp == "papel" && escolhaUsuario == "pedra")) {
+      setState(() {
+        this._mensagem = "você perdeu :(";
+        this._derrotas++;
+      });
+    } else if (escolhaUsuario == escolhaApp) {
+      setState(() {
+        this._mensagem = "Empate";
+        this._empate++;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,22 +91,13 @@ class _JogoState extends State<Jogo> {
                 textAlign: TextAlign.center,
               ),
             ),
-            GestureDetector(
-              onTap: () {
-                print("clique");
-              },
-              onDoubleTap: () {
-                print("duplo clique");
-              },
-              onLongPress: () {
-                print("long clique");
-              },
-              child: Image.asset("images/padrao.png"),
+            Image(
+              image: this._imagemApp,
             ),
             Padding(
               padding: EdgeInsets.only(top: 32, bottom: 16),
               child: Text(
-                "Escolha uma opção abaixo",
+                this._mensagem,
                 style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                 textAlign: TextAlign.center,
               ),
@@ -46,19 +105,79 @@ class _JogoState extends State<Jogo> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Image.asset(
-                  "images/papel.png",
-                  height: 100,
+                GestureDetector(
+                  onTap: () => _opcaoSelecionada("pedra"),
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundColor: this._escolhaBorderUsuario == "pedra"
+                        ? Colors.blue
+                        : Colors.grey,
+                    child: Image.asset(
+                      "images/pedra.png",
+                      height: 100,
+                    ),
+                  ),
                 ),
-                Image.asset(
-                  "images/papel.png",
-                  height: 100,
+                GestureDetector(
+                  onTap: () => _opcaoSelecionada("papel"),
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundColor: this._escolhaBorderUsuario == "papel"
+                        ? Colors.blue
+                        : Colors.grey,
+                    child: Image.asset(
+                      "images/papel.png",
+                      height: 100,
+                    ),
+                  ),
                 ),
-                Image.asset(
-                  "images/papel.png",
-                  height: 100,
+                GestureDetector(
+                  onTap: () => _opcaoSelecionada("tesoura"),
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundColor: this._escolhaBorderUsuario == "tesoura"
+                        ? Colors.blue
+                        : Colors.grey,
+                    child: Image.asset(
+                      "images/tesoura.png",
+                      height: 100,
+                    ),
+                  ),
                 ),
               ],
+            ),
+            Padding(padding: EdgeInsets.all(20)),
+            Text(
+              "Pontuação",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              "Vitórias: ${this._vitorias}",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              "Perdeu: ${this._derrotas}",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              "Empate: ${this._empate}",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: double.infinity,
+              child: RaisedButton(
+                onPressed: _resetar,
+                color: Colors.blue,
+                child: Text(
+                  "Resetar",
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
             ),
           ],
         ));
