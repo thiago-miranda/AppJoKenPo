7424921c3e8d0b9c8e4acf2cb7402201356c7fe3
thiago-miranda <thiago.miranda@hotmail.com>
2020-08-05 00:46:25 -0300

    interface

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -9,7 +9,47 @@ class _JogoState extends State<Jogo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("ok"),
-    );
+        appBar: AppBar(
+          title: Text("JoKenPo"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(
+                "Escolha do App:",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Image.asset("images/padrao.png"),
+            Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(
+                "Escolha uma opção abaixo",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Image.asset(
+                  "images/papel.png",
+                  height: 100,
+                ),
+                Image.asset(
+                  "images/papel.png",
+                  height: 100,
+                ),
+                Image.asset(
+                  "images/papel.png",
+                  height: 100,
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
